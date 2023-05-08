0d7e7991332c022dc9b9ce14c8b3fad77a41ed52
Chris <b22chrmo@student.his.se>
2023-05-08 15:28:37 +0200

    Added intent on MainActivity to view Second activity. Added method to save text, and onClick on the save button that runs the save text method. Also added another intent to return to MainActivity.

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -1,18 +1,43 @@
 package com.example.project;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        textView = (TextView) findViewById(R.id.textView2);
+
+        Button buttonMain = (Button) findViewById(R.id.button3);
+        buttonMain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        SharedPreferences sharedPreferences = getSharedPreferences(SecondActivity.editedText, MODE_PRIVATE);
+        String textSecond = sharedPreferences.getString(SecondActivity.editedText, "");
+        textView.setText(textSecond);
     }
 
 }
--- app/src/main/java/com/example/project/SecondActivity.java
+++ app/src/main/java/com/example/project/SecondActivity.java
@@ -1,5 +1,47 @@
 package com.example.project;
 
-public class SecondActivity {
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import org.w3c.dom.Text;
+
+public class SecondActivity extends AppCompatActivity {
+
+    public EditText editText;
+    public static String editedText = "Shared pref";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_second);
+
+    Button buttonSecondReturn = (Button) findViewById(R.id.button2);
+    Button buttonSecondSave = (Button) findViewById(R.id.button);
+    editText = (EditText) findViewById(R.id.edittext);
+    buttonSecondReturn.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent2 = new Intent(SecondActivity.this, MainActivity.class);
+            startActivity(intent2);
+        }
+    });
+    buttonSecondSave.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            saveText();
+
+        }
+    });
+    }
+ public void saveText() {
+     SharedPreferences sharedPref = getSharedPreferences(editedText, MODE_PRIVATE);
+     SharedPreferences.Editor editor = sharedPref.edit();
+     editor.putString(editedText, editText.getText().toString()).apply();
+ }
 }
